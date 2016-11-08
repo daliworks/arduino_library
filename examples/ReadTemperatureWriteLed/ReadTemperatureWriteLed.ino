fbf25efb21c838f917d88290c4c2a87369485491
Younghyun Jo <younghyun.jo@daliworks.net>
2016-11-08 14:18:55 +0900

    [example] sensor, actuator added

--- examples/ReadTemperatureWriteLed/ReadTemperatureWriteLed.ino
+++ examples/ReadTemperatureWriteLed/ReadTemperatureWriteLed.ino
@@ -1,24 +1,34 @@
 #include <Arduino.h>
-#include <SPI.h>
 #include <Ethernet.h> 
 
+#include <SPI.h>
+#include <Time.h>
+#include <TimeLib.h>
 #include <Timer.h>
 #include <Thingplus.h>
 
-//////////////////////////////////////////////////////////////////
-byte mac[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; 		//FIXME MAC ADDRESS
-const char *apikey = "";  					//FIXME APIKEY
-const char *ledId = "led-000000000000-0";			//FIXME LED ID
+/////////////////////////////////////////////////////////////////////////////
+byte mac[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; 			//FIXME MAC ADDRESS
+const char *apikey = "";  																//FIXME APIKEY
+const char *ledId = "led-000000000000-0";									//FIXME LED ID
 const char *temperatureId = "temperature-000000000000-0";	//FIXME TEMPERATURE ID
-//////////////////////////////////////////////////////////////////
+const char *buzzerId = "buzzer-000000000000-0";						//FIXME BUZZER ID
+const char *lightId = "light-000000000000-0";							//FIXME LIGHT ID
+const char *noiseId = "noise-000000000000-0";							//FIXME NOISE ID
+const char *buttonId = "onoff-000000000000-0";						//FIXME BUTTON ID
+/////////////////////////////////////////////////////////////////////////////
 
 Timer t;
 
 int ledOffTimer = 0;
 int ledBlinkTimer = 0;
 
+int BUTTON_GPIO = 3;
 int LED_GPIO = 8;
 int TEMP_GPIO = A0;
+int LIGHT_GPIO = A3;
+int BUZZER_GPIO = 2;
+int NOISE_GPIO = A2;
 int reportIntervalSec = 60;
 
 static EthernetClient ethernetClient;
@@ -36,13 +46,31 @@ static void _ethernetInit(void) {
 	Serial.println(Ethernet.localIP());
 }
 
-static void _gpioInit(void) {
-	pinMode(LED_GPIO, OUTPUT);
+static void _buttonISR(void) {
+	Serial.print("BUTTON PUSHED");
+	int button = digitalRead(BUTTON_GPIO);
+	Serial.println(button);
+	Thingplus.valuePublish(buttonId, button);
 }
 
-static void _ledOff() {
+static void _gpioInit(void) {
+	pinMode(LED_GPIO, OUTPUT);
+	pinMode(BUZZER_GPIO, OUTPUT);
+	pinMode(BUTTON_GPIO, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(BUTTON_GPIO), _buttonISR, CHANGE);
+}
+
+static void _ledOff(void) {
 	t.stop(ledBlinkTimer);
 	digitalWrite(LED_GPIO, LOW);
+}
+
+static void _buzzerOn(void) {
+	digitalWrite(BUZZER_GPIO, HIGH);
+}
+
+static void _buzzerOff(void) {
+	digitalWrite(BUZZER_GPIO, LOW);
 }
 
 char* actuatingCallback(const char *id, const char *cmd, JsonObject& options) {
@@ -81,6 +109,17 @@ char* actuatingCallback(const char *id, const char *cmd, JsonObject& options) {
 			return NULL;
 		}
 	}
+	else if (strcmp(id, buzzerId) == 0) {
+		if (strcmp(cmd, "on") == 0) {
+			_buzzerOn();
+		}
+		else if (strcmp(cmd, "off") == 0) {
+			_buzzerOff();
+		}
+		else {
+			return NULL;
+		}
+	}
 
 	return NULL;
 }
@@ -110,6 +149,16 @@ float temperatureGet() {
 	return temperature;
 }
 
+int lightGet() {
+	int light = analogRead(LIGHT_GPIO);
+	return light;
+}
+
+int noiseGet() {
+	int noise = analogRead(NOISE_GPIO);
+	return noise;
+}
+
 void loop() {
 	Thingplus.loop();
 	t.update();
@@ -118,9 +167,15 @@ void loop() {
 
 	if (current > nextReportInterval) {
 		Thingplus.gatewayStatusPublish(true, reportIntervalSec * 3);
-		Thingplus.sensorStatusPublish(temperatureId, true, reportIntervalSec * 3);
 		Thingplus.sensorStatusPublish(ledId, true, reportIntervalSec * 3);
+		Thingplus.sensorStatusPublish(buzzerId, true, reportIntervalSec * 3);
+		Thingplus.sensorStatusPublish(buttonId, true, reportIntervalSec * 3);
+		Thingplus.sensorStatusPublish(temperatureId, true, reportIntervalSec * 3);
 		Thingplus.valuePublish(temperatureId, temperatureGet());
+		Thingplus.sensorStatusPublish(lightId, true, reportIntervalSec * 3);
+		Thingplus.valuePublish(lightId, lightGet());
+		Thingplus.sensorStatusPublish(noiseId, true, reportIntervalSec * 3);
+		Thingplus.valuePublish(noiseId, noiseGet());
 		nextReportInterval = current + reportIntervalSec;
 	}
 }
